167918c2d3134e6cf7946fde5dcf1af70df61c89
yimnabin <yimnabin20051022@gmail.com>
2025-05-14 00:18:13 +0900

    Refactor: async startup, reset DB and Redis #2
    - main.py에 라우터 등록
    - on_startup 비동기로 처리
    - 개발 시에는 Redis, DB 자동 초기화 되도록 설정

--- app/db/init_db.py
+++ app/db/init_db.py
@@ -2,6 +2,6 @@ from sqlmodel import SQLModel
 from app.db.session import db_engine
 from app.models.user import User
 
-def create_db_and_tables():
+async def create_db_and_tables():
     SQLModel.metadata.drop_all(db_engine)
     SQLModel.metadata.create_all(db_engine)
