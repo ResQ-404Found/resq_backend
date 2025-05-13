3126363acb6eededfb39bebe84103559b641bfcf
yimnabin <yimnabin20051022@gmail.com>
2025-05-13 23:35:36 +0900

    Remove: 예제 코드 삭제
    - example.py와 테스트 용 user.py 삭제
    - main에서 example router 등록 삭제
    - init_db에서 불필요한 import 삭제

--- app/db/init_db.py
+++ app/db/init_db.py
@@ -1,6 +1,5 @@
 from sqlmodel import SQLModel
 from app.db.session import db_engine
-from app.models.user import User
 
 def create_db_and_tables():
     SQLModel.metadata.drop_all(db_engine)
--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 from fastapi import FastAPI
-from app.handlers.example import example_router
 from app.db.init_db import create_db_and_tables
 
 app = FastAPI()
 
 # Add routers
-app.include_router(example_router, prefix="/api")  
 
 # DB setup
 @app.on_event("startup")
