fc0dae73fe648f21bbc5dc05c9a78b9543ccc021
yimnabin <yimnabin20051022@gmail.com>
2025-05-17 11:41:26 +0900

    Refactor: provider 삭제 및 토큰 body에서 Header 전달로 변경

--- app/handlers/email_handler.py
+++ app/handlers/email_handler.py
@@ -1,26 +1,31 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException, Header
+from pydantic import BaseModel
 from sqlmodel import Session
 from app.db.session import get_db_session
 from app.schemas.common_schema import ApiResponse
 from app.core.redis import get_redis
 from redis.asyncio import Redis
+from app.schemas.user_schema import ResetPassword
 from app.services.email_service import EmailService
+from app.services.user_service import UserService
+from app.utils.jwt_util import JWTUtil
 
 router = APIRouter()
 
 @router.post("/request-verification-email", response_model_exclude_none=True)
 async def request_verification_email(
     email: str,
-    provider: str,
     email_service: EmailService = Depends()
 ) -> ApiResponse[None]:
-    await email_service.request_verification(email, provider)
+    await email_service.request_verification(email)
     return ApiResponse(message="인증 메일 발송 완료")
 
-@router.get("/verify-email", response_model_exclude_none=True)
+# 토큰 유효성 검사 + 이메일 인증 완료 처리
+@router.post("/verify-email-token", response_model_exclude_none=True)
 async def verify_email(
-    token: str,
+    authorization: str = Header(...),
     email_service: EmailService = Depends()
 ) -> ApiResponse[None]:
+    token = authorization.replace("Bearer ", "")
     await email_service.verify_email_token(token)
     return ApiResponse(message="이메일 인증 완료")
