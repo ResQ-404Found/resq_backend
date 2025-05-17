87b3df1c2e07de6722a809c5fcbe80b502ecf458
yimnabin <yimnabin20051022@gmail.com>
2025-05-17 11:48:52 +0900

    Feat: 비밀번호 재설정 관련 핸들러 추가 #14
    - /request-password-reset: 비밀번호 재설정 이메일 전송
    - /verify-password-reset-token: 재설정 토큰 유효성 검사
    - /reset-password: 비밀번호 재설정

--- app/handlers/email_handler.py
+++ app/handlers/email_handler.py
@@ -29,3 +29,33 @@ async def verify_email(
     token = authorization.replace("Bearer ", "")
     await email_service.verify_email_token(token)
     return ApiResponse(message="이메일 인증 완료")
+
+# 비밀번호 재설정 이메일 전송
+@router.post("/request-password-reset", response_model_exclude_none=True)
+async def request_password_reset(
+    email: str,
+    email_service: EmailService = Depends()
+)->ApiResponse[None]:
+    await email_service.request_password_reset(email)
+    return ApiResponse(message="비밀번호 재설정 메일 발송 완료")
+
+# 비밀번호 재설정
+@router.post("/reset-password", response_model_exclude_none=True)
+async def reset_password(
+    req: ResetPassword,
+    authorization: str = Header(...),
+    user_service: UserService = Depends()
+) -> ApiResponse[None]:
+    token = authorization.replace("Bearer ", "")
+    await user_service.reset_password(token, req.new_password)
+    return ApiResponse(message="비밀번호 재설정 완료")
+
+# 프론트에서 reset-password 페이지를 띄우기 전에 토큰 유효성 검사
+@router.post("/verify-password-reset-token", response_model_exclude_none=True)
+def verify_password_reset_token(authorization: str = Header(...)) -> ApiResponse[None]:
+    try:
+        token = authorization.replace("Bearer ", "")
+        JWTUtil.decode_password_reset_token(token)
+        return ApiResponse(message="토큰이 유효합니다.")
+    except Exception:
+        raise HTTPException(status_code=400, detail="토큰이 유효하지 않습니다.")
