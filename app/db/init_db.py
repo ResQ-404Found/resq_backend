ac296a23bba9af88a8cca03d88b2a62fa0ddafe1
yimnabin <yimnabin20051022@gmail.com>
2025-05-22 16:55:43 +0900

    Refactor: DB drop 함수 추가
    - 필요하면 주석 풀고 DB 리셋

--- app/db/init_db.py
+++ app/db/init_db.py
@@ -5,4 +5,7 @@ from app.schemas.user_schema import UserCreate
 from app.services.user_service import UserService
 
 async def create_db_and_tables():
+    # DB 리셋 함수, 필요에 따라 주석 해제
+    #SQLModel.metadata.drop_all(db_engine)
+    # DB 테이블 생성
     SQLModel.metadata.create_all(db_engine)
