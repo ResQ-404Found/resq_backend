b7912a71b90191036d927cd08d942badc8e68c61
yimnabin <yimnabin20051022@gmail.com>
2025-05-22 16:56:44 +0900

    Feat: Region 모델 추가 #18

---
+++ app/models/region_model.py
@@ -0,0 +1,10 @@
+from typing import List, Optional
+from sqlmodel import Field, Relationship, SQLModel
+from app.models.disasterInfo_model import DisasterInfo
+
+class Region(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    sido: str
+    sigungu: Optional[str] = None
+    eupmyeondong: Optional[str] = None
+
