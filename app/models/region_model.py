14153f079ea0fc02a435b635270e104bca775bfe
yimnabin <yimnabin20051022@gmail.com>
2025-05-22 17:03:39 +0900

    Fix: Import Error Fix

--- app/models/region_model.py
+++ app/models/region_model.py
@@ -1,6 +1,6 @@
 from typing import List, Optional
 from sqlmodel import Field, Relationship, SQLModel
-from app.models.disasterInfo_model import DisasterInfo
+#from app.models.disasterInfo_model import DisasterInfo
 
 class Region(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
