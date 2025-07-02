b1a6c2fad1871b0536f4e9750bb3c29fbc4dd890
s202455374 <yimnabin20051022@gmail.com>
2025-07-02 18:36:10 +0900

    Feat: 게시글에 이미지 업로드, 삭제 기능 추가 #36
    - create_post : 게시글 작성 및 s3 이미지 업로드
    - update_post : 게시글 이미지 url 수정 및 업데이트
    - delete_post : s3에서 이미지 삭제

--- app/handlers/post_handler.py
+++ app/handlers/post_handler.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends, HTTPException, Query
+from fastapi import APIRouter, Depends, File, Form, Query, UploadFile
 from sqlmodel import Session, select
 from typing import List, Optional
 
@@ -12,13 +12,22 @@ from app.services.post_service import PostService
 router = APIRouter()
 
 @router.post("/posts", response_model=PostRead)
-def create_post(
-    post_data: PostCreate,
+async def create_post(
+    title: str = Form(...),
+    content: str = Form(...),
+    region_id: int = Form(...),
+    files: Optional[List[UploadFile]] = File(None),
     current_user: User = Depends(get_current_user),
     session: Session = Depends(get_db_session)
 ):
+    
+    post_data = PostCreate(
+        title=title,
+        content=content,
+        region_id=region_id,
+    )
     service = PostService(session)
-    return service.create_post(post_data, current_user.id)
+    return await service.create_post(post_data, current_user.id, files)
 
 @router.get("/posts", response_model=List[PostRead])
 def read_posts(
@@ -47,14 +56,24 @@ def read_post(post_id: int, session: Session = Depends(get_db_session)):
     return service.increment_view_count(post_id)
 
 @router.patch("/posts/{post_id}", response_model=PostRead)
-def update_post(
+async def update_post(
     post_id: int,
-    post_data: PostUpdate,
+    title: Optional[str] = Form(None),  
+    content: Optional[str] = Form(None),
+    region_id: Optional[int] = Form(None),
+    post_imageURLs: Optional[List[str]] = Form(None),
+    files: Optional[List[UploadFile]] = File(None),
     current_user: User = Depends(get_current_user),
     session: Session = Depends(get_db_session)
 ):
+    post_data = PostUpdate(
+        title=title or None,
+        content=content or None,
+        region_id=region_id,
+        post_imageURLs = [url for url in post_imageURLs or [] if url] or None
+    )
     service = PostService(session)
-    return service.update_post(post_id, post_data, current_user.id)
+    return await service.update_post(post_id, post_data, current_user.id, files)
 
 @router.delete("/posts/{post_id}")
 def delete_post(
