2e2c2254df5d138615d25d02934a259eba1cea98
JIEUN <162803772+phlox22@users.noreply.github.com>
2025-07-06 21:20:14 +0900

    Feat : chatlog 모델 추가

---
+++ app/models/chatbot_model.py
@@ -0,0 +1,11 @@
+from sqlmodel import SQLModel, Field
+from typing import Optional
+from datetime import datetime
+from sqlalchemy import Column, TEXT
+
+class ChatLog(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    user_id: Optional[int] = Field(default=None, foreign_key="user.id")
+    user_message: str
+    bot_response: str = Field(sa_column=Column(TEXT))
+    created_at: datetime = Field(default_factory=datetime.utcnow)
