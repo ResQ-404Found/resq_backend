139a2b3045e283d208e7ca02824249bac3969e32
s202455374 <yimnabin20051022@gmail.com>
2025-07-06 13:00:41 +0900

    Feat: main에 스케쥴링 등록 #37

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+from sched import scheduler
 from fastapi import FastAPI
 from app.db.init_db import create_db_and_tables
-from app.handlers import user_handler, email_handler, upload_test
+from app.handlers import user_handler, email_handler
+from apscheduler.schedulers.background import BackgroundScheduler
 from app.core.redis import get_redis
 from redis.asyncio import Redis
 from app.handlers import shelter_handler
@@ -15,6 +17,7 @@ from app.handlers import like_handler
 
 
 app = FastAPI()
+scheduler = BackgroundScheduler()
 
 # Add routers
 app.include_router(shelter_handler.router, prefix="/api", tags=["shelter"])
@@ -26,6 +29,14 @@ app.include_router(post_handler.router, prefix="/api", tags=["posts"])
 app.include_router(comment_handler.router, prefix="/api", tags=["comments"])
 app.include_router(like_handler.router, prefix="/api", tags=["likes"])
 
+@scheduler.scheduled_job("interval", hours=1)
+def scheduled_disaster_fetch():
+    fetch_and_store_disasters()
+
+@scheduler.scheduled_job("cron", day=1, hour=0, minute=0)
+def scheduled_shelter_fetch():
+    fetch_and_store_shelters()
+
 # DB setup
 @app.on_event("startup")
 async def on_startup():
@@ -34,3 +45,10 @@ async def on_startup():
     await run_in_threadpool(load_region_csv)
     await run_in_threadpool(fetch_and_store_shelters)
     await run_in_threadpool(fetch_and_store_disasters)
+    scheduler.start()
+    print("[APScheduler] Started!")
+
+@app.on_event("shutdown")
+def shutdown_event():
+    scheduler.shutdown()
+    print("[APScheduler] Shutdown complete!")
