0fb07903bbd6c08a1b735ae2889d71d34b892288
s202455374 <yimnabin20051022@gmail.com>
2025-07-17 19:54:38 +0900

    Feat: Comment Model, Schema 수정 #45
    - parent_comment_id 추가
    - is_deleted 추가
    - replies로 대댓글 list 저장

--- app/models/comment_models.py
+++ app/models/comment_models.py
@@ -7,9 +7,15 @@ class Comment(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     post_id: int = Field(foreign_key="post.id")
     user_id: int = Field(foreign_key="user.id")
+    parent_comment_id: Optional[int] = Field(default=None, foreign_key="comment.id")
     content: str
     created_at: datetime = Field(default_factory=datetime.utcnow)
     last_modified: Optional[datetime] = None
     like_count: int = 0
+    is_deleted: bool = Field(default=False)
     
     likes: List["CommentLike"] = Relationship(back_populates="comment")
+    parent: Optional["Comment"] = Relationship(
+        back_populates="replies", sa_relationship_kwargs={"remote_side": "Comment.id"}
+    )
+    replies: List["Comment"] = Relationship(back_populates="parent")
